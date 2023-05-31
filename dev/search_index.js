6952443a12d7ab4685951877a2ebb557f262e809
Documenter.jl <documenter@juliadocs.github.io>
2023-05-31 16:55:20 +0000

    build based on 7dec331

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HDF5Channels","category":"page"},{"location":"#HDF5Channels","page":"Home","title":"HDF5Channels","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HDF5Channels.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HDF5Channels]","category":"page"}]
+}
